9d8d74a04826832d9dd382de9d6160e142e4f75f
Khyathi <khyathisriramyakodali293@gmail.com>
2026-04-24 09:25:36 +0530

    remove reset database button and clean UI

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 import streamlit as st
-from utils.db import create_database
 
 st.set_page_config(page_title="OLA Dashboard", layout="wide")
 
+# Main Title
 st.title("🚖 OLA Ride Insights Dashboard")
 
+# Sidebar info
 st.sidebar.success("Navigate pages from left menu")
 
-# RUN ONLY ONCE (safe)
-if st.button("🔄 Reset Database"):
-    create_database()
-    st.success("Database rebuilt successfully!")
+# Welcome section
+st.markdown("""
+### 📊 Welcome to the OLA Ride Insights Dashboard  
+This dashboard provides insights into ride trends, cancellations, revenue, and customer behavior using data analytics.
+""")
